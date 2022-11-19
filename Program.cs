d9b6d003eb2c1825deaa713874ebac1c0323f199
Igor <igorvette@mail.ru>
2022-11-19 19:36:07 +0000

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения М и N. Напишите программу, которая выведет все четные натуральные
+// числа в промежутке от М до N с ппомощью ракурсии.
+// M = 1; N = 5 -> (2,4.)
+// M = 4; N = 8 -> (4,6,8.)
+
+
+
+
+
+
+void PrintInterval(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if(m > n)
+    {
+        start = n;
+        end = m;
+    }
+    for(int i = start; i <= end; i++)
+    {
+        Console.Write($"{i%2} ");
+    }
+}
+PrintInterval(22,9);
+
+Console.WriteLine();
